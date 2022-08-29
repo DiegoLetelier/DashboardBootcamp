c3b49bc8674f94da38742290e3d0e8f80c0a7dfa
Gouki.ET <scubillosq@gmail.com>
2022-08-29 19:43:44 -0400

    CRealizo primera consulta a la api donde obtengo la data y guardo la latitud y longitud obtenida

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ const tempmin = document.getElementById('tempmin');
 const tempmax = document.getElementById('tempmax');
 const estado = document.getElementById('estado');
 const icon = document.getElementById('icon');
+
+let grados = [];
+let horas = [];
+
+async function clima(){
+    
+    let buscar = document.getElementById('buscar').value;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${buscar}&units=metric&appid=616629f9acdc3b22b8b09553e632e5da&lang=es`
+    let response = await axios.get(url)
+    response = response.data; 
+    console.log(response);
+    let lat = response.coord.lat 
+    let lon = response.coord.lon
+
+}
