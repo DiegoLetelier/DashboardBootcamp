479a099b2699f6bb9620a68fbd35ae1e8ca4e62c
Gouki.ET <scubillosq@gmail.com>
2022-08-29 20:13:44 -0400

    Añado mapa al dom

--- main.js
+++ main.js
@@ -20,6 +20,17 @@ async function clima(){
     let lat = response.coord.lat 
     let lon = response.coord.lon
 
+    let map = L.map('map').setView([`${lat}`, `${lon}`], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    
+    L.marker([`${lat}`, `${lon}`]).addTo(map)
+        .bindPopup(`Temperatura actual: ${Math.trunc(response.main.temp)}°C </br>
+        Condicion actual: ${response.weather[0].description}`)
+        .openPopup();
+    
+        
     pintaDatos(response)
 }
 
