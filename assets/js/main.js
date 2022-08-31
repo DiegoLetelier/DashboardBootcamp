682b31f2218a682436119c506966b3673fafd00e
Gouki.ET <scubillosq@gmail.com>
2022-08-31 16:23:27 -0400

    se añade mapa.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import pintaDatos from './pintaDatos.js'
+import pintaMapa from './mapa.js';
 import pintaGrafico from './grafico.js'
 const boton = document.getElementById('boton');
 
@@ -12,19 +13,22 @@ const icon = document.getElementById('icon'); */
 
 export let grados = []; //Inicia vacio para poder reasignar
 export let horas = []; //Inicia vacio para poder reasignar
+export let lat;
+export let lon;
+export let response;
 
 async function clima() {
 
     //Realizo primera consulta donde eobtengo los datos que mostrare en el dom
     let buscar = document.getElementById('buscar').value;
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${buscar}&units=metric&appid=616629f9acdc3b22b8b09553e632e5da&lang=es`
-    let response = await axios.get(url)
+    response = await axios.get(url)
     response = response.data;
     /* console.log(response); */
-    let lat = response.coord.lat //Capturo la latitud que se utilizara en la segunda consulta
-    let lon = response.coord.lon //Capturo la longitud que se utilizara en la segunda consulta    
+    /* let  */lat = response.coord.lat //Capturo la latitud que se utilizara en la segunda consulta
+    /* let  */lon = response.coord.lon //Capturo la longitud que se utilizara en la segunda consulta    
 
-    let map = L.map('map').setView([`${lat}`, `${lon}`], 13); //Crea una instancia de un objeto de mapa dado el ID DOM de un elemento <div>
+/*     let map = L.map('map').setView([`${lat}`, `${lon}`], 13); //Crea una instancia de un objeto de mapa dado el ID DOM de un elemento <div>
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //Se utiliza para cargar y mostrar capas de mosaicos en el mapa.
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map); //Añade la informacion recibida al mapa
@@ -32,8 +36,10 @@ async function clima() {
     L.marker([`${lat}`, `${lon}`]).addTo(map) //Se utiliza para mostrar iconos en los que se puede hacer clic o arrastrar en el mapa.
         .bindPopup(`Temperatura actual: ${Math.trunc(response.main.temp)}°C </br> 
         Condicion actual: ${response.weather[0].description}`) //popup que muestra la temperatura actual y el estado
-        .openPopup(); //Despliega el popup
+        .openPopup(); //Despliega el popup */
 
+
+    
     //Realizo la segunda consulta donde obtengo los datos que se utilizan en el gráfico
     let url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&units=metric&appid=616629f9acdc3b22b8b09553e632e5da&lang=es`
     let respuesta = await axios.get(url2)
@@ -50,9 +56,10 @@ async function clima() {
     
 /*     console.log("Grados: ",grados );
     console.log("Fechas: ",horas ); */
-
-    pintaGrafico();
     pintaDatos(response) //Pinto datos obtenidos de la ap
+    pintaMapa();
+    pintaGrafico();
+    
 }
 
 function obtenerHora(fecha) { //Funcion que me formatea la hora 
